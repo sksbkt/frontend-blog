264501add390107386603c661e6fab7273ebf586
atenziono <atenziono@gmail.com>
2026-07-20 14:55:27 +0330

    - fixed the RTL LTR flickering issue with a low level hotfix for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,23 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning>
+      <head>
+        {/* I FEEL GUILTY AF but if it works then it works, God I hate it xD */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                var path = window.location.pathname;
+                var isPersian = path.startsWith("/fa");
+
+                document.documentElement.lang = isPersian ? "fa" : "en";
+                document.documentElement.dir = isPersian ? "rtl" : "ltr";
+              })();
+            `,
+          }}
+        />
+      </head>
+
       <body className={`${vazir.variable} antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
